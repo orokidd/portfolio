21a4e79f99f413f3ad44ba2442a9564e57ce9992
orokidd <gregomily@gmail.com>
2026-02-04 15:55:04 +0800

    limit initial project shown

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,15 +1,20 @@
 import { projects } from "../assets/projects";
 import styles from "../styles/Projects.module.css";
 import { DemoIcon, GithubIcon } from "./Icons";
+import { useState } from "react";
 
 export default function Projects() {
-    const sortProject = [...projects].reverse()
+	const [showAll, setShowAll] = useState<boolean>(false)
+    const sortedProject = [...projects].reverse()
+	const limitedProject = sortedProject.slice(0, 9)
+
+	const showProject = showAll ? sortedProject : limitedProject
 
 	return (
 		<>
 			<h2 className={styles.projectHeading}>PROJECTS</h2>
 			<div className={styles.projects}>
-				{sortProject.map((project) => (
+				{showProject.map((project) => (
 					<div className={styles.project}>
 						<img src={project.image} alt="" className={styles.projectImage} />
 
@@ -27,6 +32,12 @@ export default function Projects() {
 						</div>
 					</div>
 				))}
+			</div>
+
+			<div className="showAllButton">
+				<button style={{color:"white"}} onClick={() => {setShowAll((prev:boolean) => !prev)}}>
+					{showAll ? "Less" : "All Projects"}
+				</button>
 			</div>
 		</>
 	);
