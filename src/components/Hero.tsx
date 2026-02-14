41c2e1a396fc50c580a01bbfb4de61985672e8d3
orokidd <gregomily@gmail.com>
2026-02-14 18:47:30 +0800

    change hero style with gsap

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,61 @@
 import portraitImage from "../assets/portrait.jpg";
-import styles from '../styles/Hero.module.css'
+import styles from "../styles/Hero.module.css";
+import { useRef } from "react";
+import { gsap } from "gsap";
 
 export default function Hero() {
+	const imageRef = useRef<HTMLImageElement>(null);
+	const heroRef = useRef<HTMLDivElement>(null)
+	
+	function moveImage(e: React.MouseEvent<HTMLDivElement>) {
+		if (!imageRef.current || !heroRef.current) return;
+
+		const imgRect = imageRef.current.getBoundingClientRect();
+  		const heroRect = heroRef.current.getBoundingClientRect();
+
+  		const x = getPosition(e.clientX, imgRect.width, heroRect.width);
+		
+		gsap.to(imageRef.current, {
+			x: x,
+			duration: 0.4,
+			ease: "power1.out",
+		});
+	}
+
+	function getPosition(cursorPosition: number, imgWidth: number, heroWidth: number) {
+		const max = heroWidth - imgWidth;
+
+		if (cursorPosition > max) {
+			return max
+		}
+
+		if (cursorPosition < 0) {
+    		return 0;
+  		}
+		
+		return cursorPosition
+	}
+
 	return (
-		<div className={styles.hero}>
-			<img src={portraitImage} alt="" className={styles.portraitImage} />
-			<div className={styles.details}>
-				<h1 className={styles.heading}>I'm Hudaa -- a full stack web developer.</h1>
-				<p className={styles.paragraph}>
+		<div className={styles.hero} onMouseMove={moveImage}>
+			<img src={portraitImage} alt="" className={styles.portraitImage} ref={imageRef} />
+			<div className={styles.details} ref={heroRef}>
+				<div className={styles.smallHero}>
+					<p>A</p>
+					<p>FULL</p>
+					<p>STACK</p>
+				</div>
+				<div className={styles.bigHero}>
+					<h1>WEB</h1>
+					<h1>DEVELOPER</h1>
+				</div>
+
+				{/* <p className={styles.paragraph}>
 					I am a self-taught full-stack developer with proficiency in JavaScript, TypeScript,
 					React, Node.js, Express.js, and relational databases such as PostgreSQL and MySQL.
 					With these technologies I am able to build a fully responsive and user intuitive full-stack web applications.
 					I am eager to apply what I've learned in real-world projects and willing to learn more as i go.
-				</p>
+				</p> */}
 			</div>
 		</div>
 	);
