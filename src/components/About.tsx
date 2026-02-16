08cf0ea03b5a067b21acedc91598c4e00f3aa8d9
orokidd <gregomily@gmail.com>
2026-02-16 14:45:42 +0800

    about me reveal animation update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,16 +11,23 @@ export default function About() {
 	const paragraphRef = useRef<HTMLParagraphElement>(null);
 
 	useEffect(() => {
+        // Split the paragraph into an array of DOM
 		const split = SplitText.create(paragraphRef.current, {
 			type: "words",
-			wordsClass: "line++",
+			wordsClass: "word++",
 		});
 
-		gsap.from(split.words, {
+        // Select every 2 words
+        const selectedWords = [];
+        for (let i = 0; i < split.words.length; i += 2) {
+            selectedWords.push(split.words[i]);
+        }
+
+		gsap.from(selectedWords, {
             scrollTrigger: {
                 trigger: paragraphRef.current,
                 start: 'top 80%',
-                end: 'center center',
+                end: 'center 30%',
                 scrub: 1,
             },
 			y: 100, // animate from 100px below
